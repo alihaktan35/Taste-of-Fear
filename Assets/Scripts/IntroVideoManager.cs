82e77dca1dcd8c08f3f9ac5b93c85c06c10d3630
Berat Biçen <rn66822x9t@privaterelay.appleid.com>
2025-12-17 22:54:44 +0300

    intro
    I created an intro that fit the game's story and integrated it into the game. When the video ends, it automatically switches to the Order 01 scene.

---
+++ Assets/Scripts/IntroVideoManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class IntroVideoManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string nextSceneName = "order01";
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    public void SkipVideo()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
