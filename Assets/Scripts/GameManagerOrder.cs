88be3513f3ed6a5e90a6a8b54ef45a704c52f0b2
alihaktan35 <alihaktan35@hotmail.com>
2025-12-18 14:03:42 +0300

    Add "Quit" button in the scene "order01" to stop/quit the game

--- Assets/Scripts/GameManagerOrder.cs
+++ Assets/Scripts/GameManagerOrder.cs
@@ -8,6 +8,7 @@ public class GameManagerOrder : MonoBehaviour
     [Header("Clown Animation Objects")]
     public GameObject speechBubble;
     public GameObject goToTableButton;
+    public GameObject quitGameButton;
     public Animator clownAnimator;
     
     private bool isWaitingForPlayer = false;
@@ -22,36 +23,47 @@ public class GameManagerOrder : MonoBehaviour
         // Başlangıçta gizle
         if (speechBubble != null) speechBubble.SetActive(false);
         if (goToTableButton != null) goToTableButton.SetActive(false);
+        if (quitGameButton != null) quitGameButton.SetActive(false);
     }
     
     public void OnClownArrived()
     {
         Debug.Log("Clown arrived!");
-        
+
         if (speechBubble != null) speechBubble.SetActive(true);
         if (goToTableButton != null) goToTableButton.SetActive(true);
-        
+        if (quitGameButton != null) quitGameButton.SetActive(true);
+
         if (clownAnimator != null)
         {
             clownAnimator.speed = 0;
         }
-        
+
         isWaitingForPlayer = true;
     }
     
     public void OnGoToTableClicked()
     {
         Debug.Log("Going to table...");
-        
+
         if (speechBubble != null) speechBubble.SetActive(false);
         if (goToTableButton != null) goToTableButton.SetActive(false);
-        
+        if (quitGameButton != null) quitGameButton.SetActive(false);
+
         if (clownAnimator != null)
         {
             clownAnimator.speed = 1;
         }
-        
+
         isWaitingForPlayer = false;
+    }
+
+    public void OnQuitGameClicked()
+    {
+        Debug.Log("Quitting to start menu...");
+
+        // startMenu sahnesine dön
+        SceneManager.LoadScene("startMenu");
     }
     
     public void OnClownExit()
